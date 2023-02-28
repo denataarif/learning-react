117a8047ff20e4ed2709aedd0699e034821c920b
Denata Arif Nur Muhamad <denataarif@gmail.com>
2023-02-28 13:30:56 +0700

    make sidemenu

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { motion } from "framer-motion";
+
 const Navigation = () => {
   const [showMenu, setShowMenu] = useState(false);
-
+  const variants = {
+    open: { opacity: 1, transform: "translateX(0)" },
+    closed: { opacity: 0, transform: "translateX(-100%)" },
+  };
+  const maskVarians = {
+    open: { opacity: 1 , transform: "translateX(0)"},
+    closed: { opacity: 0 , transform: "translateX(-100%)"},
+  };
   return (
     <>
       <nav>
@@ -13,14 +22,22 @@ const Navigation = () => {
             onClick={() => setShowMenu(!showMenu)}
           />
         </span>
-        {showMenu === true && (
-          <>
-            <div className="bg-black bg-opacity-50 fixed top-0 left-0 w-full h-full z-50" onClick={() => setShowMenu(!showMenu) }></div>
-            <div className="fixed bg-white top-0 left-0 w-4/5 h-full z-50">
-              this is menu
-            </div>
-          </>
-        )}
+          <motion.nav
+            className="bg-black bg-opacity-50 fixed top-0 left-0 w-full h-full z-50"
+            animate={showMenu ? 'open' : 'closed'}
+            variants={maskVarians}
+            onClick={() => setShowMenu(!showMenu)}
+          ></motion.nav>
+          <motion.div
+            animate={showMenu ? 'open' : 'closed'}
+            variants={variants}
+            className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 p-3"
+          >
+            <span className="font-bold">the menu</span>
+            <ul>
+              <li>Home</li>
+            </ul>
+          </motion.div>
       </nav>
     </>
   );
